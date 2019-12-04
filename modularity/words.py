8a1ae32c5a0b38745a88019b0d03e238ec5a70f0
Paolo Salazar <paolo.salazar@jalasoft.com>
2019-12-04 15:56:59 -0400

    add code improvements and fixes

--- modularity/words.py
+++ modularity/words.py
@@ -4,5 +4,8 @@ with urlopen('http://sixty-north.com/c/t.txt') as story:
     story_words = []
     for line in story:
         line_words = line.decode('utf-8').split()
-        for workd in line_words:
+        for word in line_words:
             story_words.append(word)
+
+for word in story_words:
+    print(word)
